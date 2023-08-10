d477177789779506992992bb177e402ae4c9815c
TolgaOk <tolgaokk@gmail.com>
2023-08-10 17:45:37 +0300

    Add CQL evaluation

--- examples/cql_quadrotor.py
+++ examples/cql_quadrotor.py
@@ -1,12 +1,14 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 import os
 import argparse
 from dataclasses import dataclass, asdict
-from itertools import chain
+from collections import namedtuple, defaultdict
+from itertools import chain, product
 from functools import partial
 import datetime
 import numpy as np
 import torch
+import json
 import multiprocessing
 
 from offlinerlkit.nets import MLP
@@ -188,6 +190,115 @@ def train_quadrotor_cql(n_trajectories: int, device: str, seed: int):
         eval_episodes=args.eval_episodes
     )
     policy_trainer.train()
+
+
+def make_policy(args: "Args", env: QuadrotorEnv):
+    actor_backbone = MLP(input_dim=np.prod(args.obs_shape), hidden_dims=args.hidden_dims)
+    critic1_backbone = MLP(input_dim=np.prod(args.obs_shape) +
+                           args.action_dim, hidden_dims=args.hidden_dims)
+    critic2_backbone = MLP(input_dim=np.prod(args.obs_shape) +
+                           args.action_dim, hidden_dims=args.hidden_dims)
+    dist = TanhDiagGaussian(
+        latent_dim=getattr(actor_backbone, "output_dim"),
+        output_dim=args.action_dim,
+        unbounded=True,
+        conditioned_sigma=True
+    )
+    actor = ActorProb(actor_backbone, dist, args.device)
+    critic1 = Critic(critic1_backbone, args.device)
+    critic2 = Critic(critic2_backbone, args.device)
+    actor_optim = torch.optim.Adam(actor.parameters(), lr=args.actor_lr)
+    critic1_optim = torch.optim.Adam(critic1.parameters(), lr=args.critic_lr)
+    critic2_optim = torch.optim.Adam(critic2.parameters(), lr=args.critic_lr)
+
+    policy = CQLPolicy(
+        actor,
+        critic1,
+        critic2,
+        actor_optim,
+        critic1_optim,
+        critic2_optim,
+        action_space=env.action_space,
+        tau=args.tau,
+        gamma=args.gamma,
+        alpha=args.alpha,
+        cql_weight=args.cql_weight,
+        temperature=args.temperature,
+        max_q_backup=args.max_q_backup,
+        deterministic_backup=args.deterministic_backup,
+        with_lagrange=args.with_lagrange,
+        lagrange_threshold=args.lagrange_threshold,
+        cql_alpha_lr=args.cql_alpha_lr,
+        num_repeart_actions=args.num_repeat_actions
+    )
+    return policy
+
+
+def evaluate(model: CQLPolicy, env: QuadrotorEnv, trial_seeds: List[int]):
+    eps_reward_list = []
+    for _seed in trial_seeds:
+        state = env.reset(_seed)
+        done = False
+        eps_reward = 0.0
+
+        while not done:
+            action = model.select_action(state.reshape(1, -1), deterministic=True).flatten()
+            state, reward, done, info = env.step(action)
+            eps_reward += reward
+        eps_reward_list.append(eps_reward)
+    return eps_reward_list
+
+
+def evaluate_all_cql_quadrotor_policies(experiment_path: str,
+                                        cql_seeds: Tuple[int],
+                                        data_sizes: Tuple[int],
+                                        epochs_numbers: Tuple[int],
+                                        initial_seed: int,
+                                        n_trials: int,
+                                        n_proc: int
+                                        ) -> None:
+
+    def _evaluate_epochs(cql_seed: int, data_size: int) -> None:
+        path = os.path.join(experiment_path, f"seed_{cql_seed}-size_{data_size}/")
+        latest_experiment_name = sorted(os.listdir(path))[-1]
+
+        checkpoint_dir = os.path.join(path, latest_experiment_name, "checkpoint")
+        argument_path = os.path.join(path, latest_experiment_name, "record", "hyper_param.json")
+        with open(argument_path, "r") as fobj:
+            arg_dict = json.load(fobj)
+            Args = namedtuple("Args", ",".join(arg_dict.keys()))
+            args = Args(**arg_dict)
+        args
+
+        seed_rng = np.random.default_rng(initial_seed)
+        trial_seeds = [seed.item() for seed in seed_rng.integers(0, 2**30, n_trials)]
+        env = QuadrotorEnv(use_exp_reward=True)
+        env = OldSchoolWrapper(env, score_range=(0, 300))
+
+        model_evaluations = {}
+        model = make_policy(args, env)
+        for index in epochs_numbers:
+            model.load_state_dict(torch.load(
+                os.path.join(checkpoint_dir, "policy_{0:04d}.pth".format(index))))
+            model_evaluations[index] = evaluate(model, env, trial_seeds)
+        return model_evaluations
+
+    key_pairs = list(product(cql_seeds, data_sizes))
+    evaluations = parallelize(
+        n_proc=n_proc,
+        fn=_evaluate_epochs,
+        kwargs_list=[
+            dict(cql_seed=cql_seed, data_size=data_size)
+            for cql_seed, data_size in key_pairs
+        ],
+        loading_bar_kwargs=dict(desc="Progress")
+    )
+
+    cql_evaluations = defaultdict(dict)
+    for eval_scores, (cql_seed, data_size) in zip(evaluations, key_pairs):
+        cql_evaluations[data_size][cql_seed] = eval_scores
+
+    return cql_evaluations
 
 
 if __name__ == "__main__":
