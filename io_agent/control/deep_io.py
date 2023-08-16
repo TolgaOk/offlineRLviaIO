33f6e6379045e01cf11deea857b96267636cf549
TolgaOk <tolgaokk@gmail.com>
2023-08-16 16:21:42 +0300

    Update deep IO with cuda and deep layers that are not tested yet

--- io_agent/control/deep_io.py
+++ io_agent/control/deep_io.py
@@ -24,6 +24,7 @@ class IterativeIOController(torch.nn.Module, IOController):
                  state_constraints_flag: bool = True,
                  learning_rate: float = 1e-4,
                  lr_exp_decay: float = 0.98,
+                 device: str = "cpu",
                  ):
         super().__init__()
         self.learning_rate = learning_rate
@@ -41,9 +42,24 @@ class IterativeIOController(torch.nn.Module, IOController):
             soften_state_constraints=True,
             softening_penalty=1e9,
         )
-
+        self.device = device
         self.scheduler = torch.optim.lr_scheduler.ExponentialLR(
             self.train_optimizer, gamma=np.sqrt(lr_exp_decay).item())
+        self.to(device)
+
+        self.const_matrix = self.params.constraints.action.matrix
+        self.const_vector = self.params.constraints.action.vector
+
+        if self.state_constraints_flag:
+            raise NotImplementedError("Iterative IO Controller does not support state constraints!")
+
+        if (len(self.const_vector) == 2 * self.action_size
+            and np.allclose(self.const_matrix[:self.action_size], np.eye(self.action_size))
+                and np.allclose(self.const_matrix[self.action_size:], -np.eye(self.action_size))):
+            self.action_high = self.to_torch(self.const_vector[:self.action_size].reshape(1, -1))
+            self.action_low = -self.to_torch(self.const_vector[self.action_size:].reshape(1, -1))
+        else:
+            raise NotImplementedError("Only rectengular action constraints are supported!")
 
     @staticmethod
     def softplus_epsilon(x, epsilon=1e-6):
@@ -57,16 +73,35 @@ class IterativeIOController(torch.nn.Module, IOController):
             f=(self.softplus_epsilon, inv_softplus_epsilon),
             triv="expm"
         )
+
+        # self.theta_su_net = torch.nn.Sequential(
+        #     torch.nn.Linear(self.aug_state_size, 128),
+        #     torch.nn.Elu(),
+        #     torch.nn.Linear(128, 128),
+        #     torch.nn.Elu(),
+        #     torch.nn.Linear(128, self.action_size * self.aug_state_size),
+        # )
+        # self.theta_uu_net = torch.nn.Sequential(
+        #     torch.nn.Linear(self.aug_state_size, 128),
+        #     torch.nn.Elu(),
+        #     torch.nn.Linear(128, 128),
+        #     torch.nn.Elu(),
+        #     torch.nn.Linear(128, (self.action_size ** 2)),
+        #     torch.nn.Unflatten(-1, (self.action_size, self.action_size)),
+        #     geotorch.PSD((self.action_size, self.action_size),
+        #                  f=(self.softplus_epsilon, inv_softplus_epsilon),
+        #                  triv="expm")
+        # )
         self.th_theta_su = torch.nn.Linear(self.action_size, self.aug_state_size, bias=False)
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
     def q_fn(self,
              state: torch.Tensor,
              action: torch.Tensor,
-             theta_uu: Optional[torch.Tensor]
+             theta_uu: torch.Tensor,
+             theta_su: torch.Tensor,
              ) -> torch.Tensor:
-        theta_uu = theta_uu if theta_uu is not None else self.th_theta_uu.weight
-        return (2 * (state * (self.th_theta_su.weight @ action.T).T).sum(dim=-1)
+        return (2 * (state * (theta_su @ action.T).T).sum(dim=-1)
                 + (action * (theta_uu @ action.T).T).sum(dim=-1))
 
     def batch_minimizer_actions(self,
@@ -74,39 +109,54 @@ class IterativeIOController(torch.nn.Module, IOController):
                                 theta_uu: torch.Tensor,
                                 theta_su: torch.Tensor,
                                 ) -> torch.Tensor:
-        if self.state_constraints_flag:
-            raise NotImplementedError("Iterative IO Controller does not support state constraints!")
-
         min_actions = -(torch.linalg.inv(theta_uu) @ theta_su.T @ aug_states.T).T
-        if (len(self.params.constraints.action.vector) == 2 * self.action_size
-            and np.allclose(self.params.constraints.action.matrix[:self.action_size], np.eye(self.action_size))
-                and np.allclose(self.params.constraints.action.matrix[self.action_size:], -np.eye(self.action_size))):
-            action_high = torch.from_numpy(self.params.constraints.action.vector[:self.action_size].reshape(
-                1, -1)).float().to(aug_states.device)
-            action_low = -torch.from_numpy(self.params.constraints.action.vector[self.action_size:].reshape(
-                1, -1)).float().to(aug_states.device)
-            min_actions = min_actions.clamp(
-                min=action_low,
-                max=action_high)
-        else:
-            raise NotImplementedError("Only rectengular action constraints are supported!")
-
+        min_actions = min_actions.clamp(
+            min=self.action_low,
+            max=self.action_high)
         return min_actions
+
+    def compute(self,
+                state: np.ndarray,
+                reference: np.ndarray,
+                ) -> Tuple[Union[np.ndarray, float]]:
+        if self._past_state is not None and self._past_action is not None:
+            self._history = self.feature_handler.update_history(
+                state=self._past_state,
+                action=self._past_action,
+                next_state=state,
+                history=self._history)
+        th_aug_state = self.to_torch(self.feature_handler.augment_state(
+            state, self._history).reshape(1, -1))
+        kwargs = dict(aug_states=th_aug_state,
+                      theta_su=self.th_theta_su.weight,
+                      theta_uu=self.th_theta_uu.weight)
+        th_action = self.batch_minimizer_actions(**kwargs)
+        action = th_action.detach().cpu().numpy().reshape(-1)
+        self._past_state = state.copy()
+        self._past_action = action.copy()
+        return action, None
 
     def loss(self, aug_states: torch.Tensor, exp_action: torch.Tensor) -> torch.Tensor:
         theta_uu = self.th_theta_uu.weight
-        min_actions = self.batch_minimizer_actions(aug_states, theta_uu, self.th_theta_su.weight)
-        return (self.q_fn(aug_states, exp_action, theta_uu=theta_uu)
-                - self.q_fn(aug_states, min_actions, theta_uu=theta_uu))
+        min_actions = self.batch_minimizer_actions(
+            aug_states, theta_uu=theta_uu, theta_su=self.th_theta_su.weight)
+        return (self.q_fn(aug_states, exp_action, theta_uu=theta_uu, theta_su=self.th_theta_su.weight)
+                - self.q_fn(aug_states, min_actions, theta_uu=theta_uu, theta_su=self.th_theta_su.weight))
+
+    def to_torch(self, array: np.ndarray) -> torch.Tensor:
+        return torch.from_numpy(array).float().to(self.device)
 
     def train(self,
               augmented_dataset: List[AugmentedTransition],
               epochs: int,
               batch_size: int,
               rng: np.random.Generator,
-              verbose: bool = True
+              verbose: bool = True,
               ) -> None:
         train_losses = []
+
+        th_aug_states = self.to_torch(np.stack([tran.aug_state for tran in augmented_dataset]))
+        th_exp_actions = self.to_torch(np.stack([tran.expert_action for tran in augmented_dataset]))
 
         with tqdm(total=epochs, disable=not verbose) as pbar:
             for epoch in range(epochs):
@@ -117,14 +167,9 @@ class IterativeIOController(torch.nn.Module, IOController):
                 for index in range(0, data_size, batch_size):
                     indices = dataset_indices[index: index + batch_size]
 
-                    aug_states = np.stack(
-                        [augmented_dataset[_index].aug_state for _index in indices], axis=0)
-                    exp_actions = np.stack(
-                        [augmented_dataset[_index].expert_action for _index in indices], axis=0)
-
                     loss = self.loss(
-                        torch.from_numpy(aug_states).float(),
-                        torch.from_numpy(exp_actions).float()
+                        th_aug_states[indices],
+                        th_exp_actions[indices]
                     ).mean(-1)
 
                     self.train_optimizer.zero_grad()
